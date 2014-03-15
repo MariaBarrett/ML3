a0a18f70505b9847b052282899f130e5d8b7d711
MariaBarrett <Maria@Macintosh-5.local>
2014-03-15 18:12:39 +0100

    now with train results

--- OurSVM.py
+++ OurSVM.py
@@ -65,10 +65,11 @@ def transformtest(trainset, testset):
 
 """
 This function splits the shuffled train set in s equal sized splits. 
+It expects the features, the labels and number of slices. 
 It starts by making a copy of the labels and features and shuffles them. The lambda constant makes sure that it's always shuffled the same way 
 It returns a list of s slices containg lists of datapoints belonging to s.
 """
-def sfold(labels, features,s):
+def sfold(features, labels, s):
 	featurefold = np.copy(features)
 	labelfold = np.copy(labels)
 
@@ -85,28 +86,26 @@ def sfold(labels, features,s):
 ##############################################################################
 
 """
-After having decorated, the function gets all combinations of C's and gammas. For each combination it
+The function expects a train set with labels and number of folds. 
+The function has all C's and gammas. For each combination it
 runs 5 fold crossvalidation: 
 For every test-set for as many folds as there are: use the remaining as train sets exept if it's the test set. 
 Then we sum up the result for every run and average it. The average performance per combination is stored.
 The best /lowest average and the combination that produced it is returned.   
 """
-def crossval(X, y, folds):
+def crossval(X_train, y_train, folds):
 	# Set the parameters by cross-validation
-	tuned_parameters = [{'gamma': [0.00001,0.0001,0.001,0.01,0.1,1],
+	tuned_parameters = [{'gamma': [0.00000000001, 0.0000000001,0.000000001, 0.00000001, 0.0000001, 0.000001,0.00001,0.0001,0.001,0.01,0.1,1],
                      'C': [0.001,0.01,0.1,1,10,100]}]
-
-	print ('-'*45)
-	print ('%d-fold cross validation' %folds)
-	print ('-'*45)
 	
-	labels_slices, features_slices = sfold(y, X,folds)
+	labels_slices, features_slices = sfold(X_train, y_train, folds)
 	accuracy = []
 
 	#gridsearch
 	for g in tuned_parameters[0]['gamma']:
 		for c in tuned_parameters[0]['C']:
 			temp = []
+			tr_temp = []
 			#crossvalidation
 			for f in xrange(folds):
 				crossvaltrain = []
@@ -125,16 +124,24 @@ def crossval(X, y, folds):
 							
 						for lab in labels_slices[i]:
 							crossvaltrain_labels.append(lab) #...and a list of adjacent labels
-				
 				crossvaltrain = np.array(crossvaltrain)
 				crossvaltrain_labels = np.array(crossvaltrain_labels)
 
 				#Classifying using libsvm
-				out = libsvm.fit(crossvaltrain, crossvaltrain_labels, svm_type=0, C=g, gamma=g)
+				out = libsvm.fit(crossvaltrain, crossvaltrain_labels, svm_type=0, C=c, gamma=g)
+				train_y_pred = libsvm.predict(crossvaltrain, *out)
 				y_pred = libsvm.predict(crossvaltest, *out)
-				#getting the error count
-				counter = 0
 				
+
+				#getting the train error count
+				tr_count = 0
+				for l in xrange(len(crossvaltrain_labels)):
+					if train_y_pred[l] != crossvaltrain_labels[l]:
+						tr_count +=1
+				tr_temp.append(tr_count / len(crossvaltrain))
+
+				#getting the test error count
+				counter = 0
 				for y in xrange(len(y_pred)):
 					if y_pred[y] != crossvaltest_labels[y]:
 						counter +=1
@@ -142,28 +149,39 @@ def crossval(X, y, folds):
 				temp.append(counter / len(crossvaltrain))
 
 			#for every setting, get the average performance of the 5 runs:
-			temp = np.array(temp)
-			mean = np.mean(temp)
-			print "Average error of %s: %.6f" %((c,g), mean)
-			accuracy.append([c,g,mean])
+			trainmean = np.array(np.mean(tr_temp))
+			testmean = np.array(np.mean(temp))
+			print "Average test error of %s: %.6f" %((c,g), testmean)
+			accuracy.append([c,g,testmean, trainmean])
 
 	#After all C's and gammas have been tried: get the best performance and the hyperparam pairs for that:
 	accuracy.sort(key=itemgetter(2)) #sort by error - lowest first
-	bestperf = accuracy[0][-1]
+	bestperf = accuracy[0][-2]
+	besttrain = accuracy[0][-1]
 	bestpair = tuple(accuracy[0][:2])
-	print "\nAverage error of best hyperparameter (C, gamma), %s: %.4f" %(bestpair, bestperf)
-
+	print "\nAverage error of best hyperparameter (C, gamma), %s: Test = %.6f train = %.6f" %(bestpair, bestperf, besttrain)
+	return bestpair
 
 def error_svc(X_train, y_train, X_test, y_test):
-	out = libsvm.fit(X_train, y_train, svm_type=0, C=1, gamma=0.001)
+	out = libsvm.fit(X_train, y_train, svm_type=0, C=best_hyperparam_norm[0], gamma=best_hyperparam_norm[1])
+	train_y_pred = libsvm.predict(X_test, *out)
 	y_pred = libsvm.predict(X_test, *out)
 
+	#train error
+	c = 0
+	for y in xrange(len(train_y_pred)):
+		if train_y_pred[y] != y_train[y]:
+			c +=1
+	train_error = c / len(X_train)
+
+
+	#test error
 	counter = 0
 	for y in xrange(len(y_pred)):
 		if y_pred[y] != y_test[y]:
 			counter +=1
-	error = counter / len(X_test)
-	return error
+	test_error = counter / len(X_test)
+	return train_error, test_error
 	
 """
 This function tries fitting using different C's and looks at the output. 
@@ -172,11 +190,11 @@ The rest of the support vectors are free.
 It prints the number of free and bound support vectors. 
 """
 def differentC(X_train, y_train, X_test, y_test):
-	C = [1,10,100,1000,10000]
+	C = [0.01, 0.1, 1,10,100,1000,10000,100000]
 	
 	for c in C:
 		bounded = 0
-		out = libsvm.fit(X_train, y_train, C=c, gamma=0.001)	
+		out = libsvm.fit(X_train, y_train, C=c, gamma=gamma_difc)	
 
 		supportvectors = len(out[0])
 		coef = out[3]
@@ -184,7 +202,7 @@ def differentC(X_train, y_train, X_test, y_test):
 			if co == c:
 				bounded += 1
 		free = supportvectors - bounded
-		print "C = %d: free: %d, bounded: %d " %(c, free, bounded)
+		print "C = %.2f: free: %d, bounded: %d " %(c, free, bounded)
 	
 
  ##############################################################################
@@ -210,27 +228,36 @@ print "Mean of transformed test set: \n", transformtest_mean
 print "Variance of transformed test set: \n", transformtest_var
 
 print '*'*45
-print "Not normalized"
+print "Raw"
 print '*'*45
+print '-'*45
+print '5-fold cross validation'
+print '-'*45
+print '(C, gamma)'
 best_hyperparam = crossval(X_train, y_train, 5)
 
 print '*'*45
 print "Normalized"
 print '*'*45
+print '-'*45
+print '5-fold cross validation'
+print '-'*45
+print 'C, gamma'
 best_hyperparam_norm = crossval(X_train_norm, y_train, 5)
 
 print '*'*45
-print "Error when trained on train set tested on test using best hyperparameter"
+print "Error when trained on train set tested on test using best hyperparameter (C,gamma):",best_hyperparam_norm
 print '*'*45
 
-err = error_svc(X_train, y_train, X_test, y_test)
-print "Not normalized: ", err
+train_err, test_err = error_svc(X_train, y_train, X_test, y_test)
+print "Raw: train = %.6f test = %.6f " % (train_err, test_err)
 
-err_norm = error_svc(X_train_norm, y_train, X_test_trans, y_test)
-print "Normalized: ", err_norm
+train_err_norm, test_err_norm = error_svc(X_train_norm, y_train, X_test_trans, y_test)
+print "Normalized / transformed: train = %.6f, test =%.6f" %(train_err_norm, test_err_norm)
 		
+gamma_difc = 0.0001
 print '*'*45
-print "Number of free and bounded support vectors with different C, gamma = 0.001"
+print "Number of free and bounded support vectors with different C, gamma =",gamma_difc
 print '*'*45	
 
-differentC(X_train, y_train, X_test, y_test)
+differentC(X_train_norm, y_train, X_test_trans, y_test)
